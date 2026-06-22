69d7b314e13168383e4a2e08d0cb6a6978f2e625
MohdMuttavaquil <mmuttavaquil@gmail.com>
2026-06-22 17:33:14 +0530

    Add server url

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,9 @@ export const AppProvider = ({ children }) =>{
    const [titleId, setTitleId] = useState()
    const [userInfo, setUserInfo] = useState()
    const [chat, setChat] = useState([])
-   const url = 'http://localhost:3000'
+   const url = 'https://hpz7blqb44kkjzogumd642dole0lbwou.lambda-url.ap-south-1.on.aws'
+  // const url = 'http://localhost:3000'
+
 
   return(
     <AppContext.Provider value={{ titleId, setTitleId, userInfo, setUserInfo, chat, url, setChat}} >
