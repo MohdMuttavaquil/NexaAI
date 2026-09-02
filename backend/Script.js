a178daeac7af44813f6a53b12ed0be4112a11a28
MohdMuttavaquil <mmuttavaquil@gmail.com>
2026-09-02 17:45:40 +0530

    add new origin

--- backend/Script.js
+++ backend/Script.js
@@ -9,7 +9,8 @@ import cookieParser from "cookie-parser";
 
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:5173',
+   // origin: 'http://localhost:5173',
+   origin: 'https://nexa-ai-three.vercel.app',
     credentials: true
 }))
 
