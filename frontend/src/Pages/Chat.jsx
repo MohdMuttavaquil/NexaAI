720e30a528aba93f9ccf489756a0c602df3d3fac
MohdMuttavaquil <mmuttavaquil@gmail.com>
2026-09-19 16:56:08 +0530

    fix erro

--- frontend/src/Pages/Chat.jsx
+++ frontend/src/Pages/Chat.jsx
@@ -102,7 +102,7 @@ const Chat = () => {
 
                     <form className='my-2 flex justify-center gap-2 mx-auto'>
 
-                        <input type='text' value={message} placeholder='how can i help you today' onChange={(e) => setMessage(e.target.value)} className='outline-none rounded-2xl px-2 border-b-white border-b-2 md:w-[60%] w-[80%] py-2' required/>
+                        <input type='text' value={message} placeholder='how can i help you today' onChange={(e) => setMessage(e.target.value)} className='outline-none rounded-2xl px-2 border-b-white border-b-2 md:w-[60%] w-[80%] py-2'/>
 
                         <button disabled={isLoading} onClick={() => show()} className='py-2 bg-black px-3 rounded-2xl cursor-pointer'><LuSend /> </button>
                     </form>
